eddbf6962dedd9b5a526610aa3f19a9e9a1fb7bf
AbdulrahmanLuqman <luqmanola60@gmail.com>
2025-10-05 06:50:06 +0100

    fix: max width added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} antialiased`}
+        className={`${poppins.variable} antialiased max-w-[1440px] mx-auto`}
       >
         <Navbar />
         {children}
